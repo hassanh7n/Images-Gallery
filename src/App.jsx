e9bdf1d015d0971a2ed6b7843c93954e3b074cfb
unknown <hassanwebdeveloper8322@gmail.com>
2023-05-11 16:59:07 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ThemeToggle from "./ThemeToggle";
+import SearchForm from './SearchForm'
+import Gallery from './Gallery'
+import Footer from "./Footer";
+const App = () => {
+  return <h1>
+    <ThemeToggle />
+    <SearchForm />
+    <Gallery />
+    <Footer />
+  </h1>;
+};
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+        <p>Copyright &copy; Unsplash </p>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
